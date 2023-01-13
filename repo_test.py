159631e8082574b81cb25fccacb88dec29c0ffad
aelnar <61490301+aelnar@users.noreply.github.com>
2023-01-13 09:10:54 -0800

    all conditions

--- repo_test.py
+++ repo_test.py
@@ -1,3 +1,5 @@
+# fcns from gitpython doc -> https://gitpython.readthedocs.io/en/stable/tutorial.html
+
 import git
 import time
 
@@ -16,7 +18,17 @@ if len(repo.index.diff(None)) > 0:
     local_change = True
 print("local changes: " + str(local_change))
 
-# get commit date
+# get commit date using commit object
+# headcommit.authored_date -> seconds since epoch
+# check if commited date is anywhere within seconds of last week
+recent_commit = False
 headcommit = repo.head.commit
-print(headcommit.committed_date)
-print(headcommit.authored_date)
+if (headcommit.authored_date <= headcommit.committed_date <= (headcommit.authored_date + 604800)):
+    recent_commit = True
+print("recent commit: " + str(recent_commit))
+
+# author name of head commit
+rufus = False
+if(headcommit.author.name == "Rufus"):
+    rufus = True
+print("blame Rufus: " + str(rufus))
