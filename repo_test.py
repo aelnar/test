c6f8e52a1b4492f79e04dd89aba46595c4b6d07c
aelnar <61490301+aelnar@users.noreply.github.com>
2023-01-13 09:01:47 -0800

    more stuff

--- repo_test.py
+++ repo_test.py
@@ -1,4 +1,22 @@
-from git import Repo
+import git
+import time
 
-repo = Repo(self.rorepo.working_tree_dir)
-print(repo)
+# repo.working_tree_dir -> repo link (local)
+repo = git.Repo(".", search_parent_directories=True)
+
+# active branch
+active_branch = repo.active_branch
+print("active branch: " + active_branch.name)
+
+# diff framework -> .index.diff
+# gitpython doc -> "A diff between the index and the working tree"
+# if there's anything in the list -> a file has been changed but not commited
+local_change = False
+if len(repo.index.diff(None)) > 0:
+    local_change = True
+print("local changes: " + str(local_change))
+
+# get commit date
+headcommit = repo.head.commit
+print(headcommit.committed_date)
+print(headcommit.authored_date)
