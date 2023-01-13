87fb2c15cba5102be2098dbb809d707dd33fbe17
aelnar <61490301+aelnar@users.noreply.github.com>
2023-01-13 09:14:37 -0800

    made git_dir_fcn with git_dir param

--- repo_test.py
+++ repo_test.py
@@ -1,34 +1,35 @@
 # fcns from gitpython doc -> https://gitpython.readthedocs.io/en/stable/tutorial.html
-
 import git
-import time
 
-# repo.working_tree_dir -> repo link (local)
-repo = git.Repo(".", search_parent_directories=True)
+def git_dir_fcn(git_dir):
 
-# active branch
-active_branch = repo.active_branch
-print("active branch: " + active_branch.name)
+    # repo.working_tree_dir -> repo link (local)
+    # takes param
+    repo = git.Repo(git_dir)
 
-# diff framework -> .index.diff
-# gitpython doc -> "A diff between the index and the working tree"
-# if there's anything in the list -> a file has been changed but not commited
-local_change = False
-if len(repo.index.diff(None)) > 0:
-    local_change = True
-print("local changes: " + str(local_change))
+    # active branch
+    active_branch = repo.active_branch
+    print("active branch: " + active_branch.name)
 
-# get commit date using commit object
-# headcommit.authored_date -> seconds since epoch
-# check if commited date is anywhere within seconds of last week
-recent_commit = False
-headcommit = repo.head.commit
-if (headcommit.authored_date <= headcommit.committed_date <= (headcommit.authored_date + 604800)):
-    recent_commit = True
-print("recent commit: " + str(recent_commit))
+    # diff framework -> .index.diff
+    # gitpython doc -> "A diff between the index and the working tree"
+    # if there's anything in the list -> a file has been changed but not commited
+    local_change = False
+    if len(repo.index.diff(None)) > 0:
+        local_change = True
+    print("local changes: " + str(local_change))
 
-# author name of head commit
-rufus = False
-if(headcommit.author.name == "Rufus"):
-    rufus = True
-print("blame Rufus: " + str(rufus))
+    # get commit date using commit object
+    # headcommit.authored_date -> seconds since epoch
+    # check if commited date is anywhere within seconds of last week
+    recent_commit = False
+    headcommit = repo.head.commit
+    if (headcommit.authored_date <= headcommit.committed_date <= (headcommit.authored_date + 604800)):
+        recent_commit = True
+    print("recent commit: " + str(recent_commit))
+
+    # author name of head commit
+    rufus = False
+    if(headcommit.author.name == "Rufus"):
+        rufus = True
+    print("blame Rufus: " + str(rufus))
